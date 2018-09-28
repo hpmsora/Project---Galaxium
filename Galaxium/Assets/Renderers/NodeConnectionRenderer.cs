68e9fd1d7804b887a37bd23d64c4942dca8091a9
Won Yong Ha <hpmsora@gmail.com>
2018-09-28 16:51:38 -0400

    Connection with GL.Line. Line built but not showing on Game Playing Scene.

---
+++ Galaxium/Assets/Renderers/NodeConnectionRenderer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NodeConnectionRenderer : MonoBehaviour {
+
+    public Material LineMat;
+
+    public GameObject GameObject_ParentNode;
+    public GameObject GameObject_ChildNode;
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void DrawNodeConnection() {
+        Vector3 Parent_Position = GameObject_ParentNode.transform.position;
+        Vector3 Child_Position = GameObject_ChildNode.transform.position;
+
+        GL.Begin(GL.LINES);
+
+        LineMat.SetPass(0);
+
+        GL.Color(new Color(LineMat.color.g, LineMat.color.g, LineMat.color.b, LineMat.color.a));
+        GL.Vertex(Parent_Position);
+        GL.Vertex(Child_Position);
+        GL.End();
+    }
+
+    void OnRenderObject()
+    {
+        DrawNodeConnection();
+    }
+
+    void OnDrawGizmos()
+    {
+        DrawNodeConnection();
+    }
+}
